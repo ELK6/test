c6e44f25c8ae56dd652e3329835406343b658d27
tr <2187488063@qq.com>
2021-02-04 14:44:56 +0800

    初始化

---
+++ src/views/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about">
+    <h2>vue3</h2>
+    <div>
+      <p>{{ count }}</p>
+      <button @click="addClick">+1</button>
+      <ul>
+        <li v-for="(item,index) in state.lists" :key="index">
+            {{item.name}}
+         </li>
+      </ul>
+      <button @click="send">发送数据</button>
+    </div>
+  </div>
+</template>
+<script>
+import { reactive, ref } from "vue";
+import emitter from '../utils/mitt'
+export default {
+  setup() {
+    const count = ref(50);
+    const addClick = () => {
+      count.value++;
+    };
+    const state = reactive({
+      lists: [
+        {
+          name: "ime",
+          age: 20,
+        },
+      ],
+    });
+    return {
+      count,
+      addClick,
+      state,
+    };
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h3>vue2</h3>
+    <div>
+      <p>{{ count }}</p>
+      <button @click="addClickHandle">+1</button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      count: 100,
+    };
+  },
+  components: {},
+  methods: {
+    addClickHandle() {
+      this.count++
+    },
+  },
+};
+</script>
